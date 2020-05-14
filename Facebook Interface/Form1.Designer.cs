208b6ae85a7f8abaed70b295414c106408c29626
hcobert <55706736+hcobert@users.noreply.github.com>
2020-05-14 14:08:34 +0100

    Final Version

---
+++ Facebook Interface/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace Facebook_Interface
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridProfiles = new System.Windows.Forms.DataGridView();
+            this.UserIDBox = new System.Windows.Forms.TextBox();
+            this.HometownBox = new System.Windows.Forms.TextBox();
+            this.GenderBox = new System.Windows.Forms.TextBox();
+            this.CurrentLocationBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.FirstNameBox = new System.Windows.Forms.TextBox();
+            this.LastNameBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridProfiles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridProfiles
+            // 
+            this.dataGridProfiles.AllowUserToAddRows = false;
+            this.dataGridProfiles.AllowUserToDeleteRows = false;
+            this.dataGridProfiles.AllowUserToResizeColumns = false;
+            this.dataGridProfiles.AllowUserToResizeRows = false;
+            this.dataGridProfiles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridProfiles.Location = new System.Drawing.Point(12, 12);
+            this.dataGridProfiles.Name = "dataGridProfiles";
+            this.dataGridProfiles.RowHeadersVisible = false;
+            this.dataGridProfiles.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataGridProfiles.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridProfiles.Size = new System.Drawing.Size(135, 239);
+            this.dataGridProfiles.TabIndex = 0;
+            this.dataGridProfiles.SelectionChanged += new System.EventHandler(this.dataGridProfiles_SelectionChanged);
+            // 
+            // UserIDBox
+            // 
+            this.UserIDBox.Enabled = false;
+            this.UserIDBox.Location = new System.Drawing.Point(346, 14);
+            this.UserIDBox.Name = "UserIDBox";
+            this.UserIDBox.Size = new System.Drawing.Size(100, 20);
+            this.UserIDBox.TabIndex = 2;
+            // 
+            // HometownBox
+            // 
+            this.HometownBox.Location = new System.Drawing.Point(346, 92);
+            this.HometownBox.Name = "HometownBox";
+            this.HometownBox.Size = new System.Drawing.Size(100, 20);
+            this.HometownBox.TabIndex = 3;
+            // 
+            // GenderBox
+            // 
+            this.GenderBox.Location = new System.Drawing.Point(346, 118);
+            this.GenderBox.Name = "GenderBox";
+            this.GenderBox.Size = new System.Drawing.Size(100, 20);
+            this.GenderBox.TabIndex = 4;
+            // 
+            // CurrentLocationBox
+            // 
+            this.CurrentLocationBox.Location = new System.Drawing.Point(346, 144);
+            this.CurrentLocationBox.Name = "CurrentLocationBox";
+            this.CurrentLocationBox.Size = new System.Drawing.Size(100, 20);
+            this.CurrentLocationBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(255, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "UserID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(255, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Hometown";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(255, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Gender";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(255, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Current Location";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(357, 199);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Clear Selection";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(357, 170);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(89, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Save Changes";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(357, 228);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(89, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Add User";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // FirstNameBox
+            // 
+            this.FirstNameBox.Location = new System.Drawing.Point(346, 40);
+            this.FirstNameBox.Name = "FirstNameBox";
+            this.FirstNameBox.Size = new System.Drawing.Size(100, 20);
+            this.FirstNameBox.TabIndex = 13;
+            // 
+            // LastNameBox
+            // 
+            this.LastNameBox.Location = new System.Drawing.Point(346, 66);
+            this.LastNameBox.Name = "LastNameBox";
+            this.LastNameBox.Size = new System.Drawing.Size(100, 20);
+            this.LastNameBox.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(255, 43);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "First Name";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(255, 69);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Last Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 263);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.LastNameBox);
+            this.Controls.Add(this.FirstNameBox);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CurrentLocationBox);
+            this.Controls.Add(this.GenderBox);
+            this.Controls.Add(this.HometownBox);
+            this.Controls.Add(this.UserIDBox);
+            this.Controls.Add(this.dataGridProfiles);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridProfiles)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridProfiles;
+        private System.Windows.Forms.TextBox UserIDBox;
+        private System.Windows.Forms.TextBox HometownBox;
+        private System.Windows.Forms.TextBox GenderBox;
+        private System.Windows.Forms.TextBox CurrentLocationBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox FirstNameBox;
+        private System.Windows.Forms.TextBox LastNameBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Facebook Interface/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Facebook_Interface
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection connection;
+        MySqlCommand command;
+        MySqlDataAdapter adapter;
+        DataTable table;
+        int userID;
+        string firstName;
+        string lastName;
+        int nextID;
+
+        public void CreateDataGrid()
+        {
+            connection = new MySqlConnection("Server = proj-mysql.uopnet.plymouth.ac.uk; Port = 3306; UID = ISAD157_HCobert; password = ISAD157_22227228; database = isad157_hcobert; SslMode = none");
+            command = new MySqlCommand("SELECT UserID, FirstName, LastName FROM user;", connection);
+            connection.Open();
+            adapter = new MySqlDataAdapter(command);
+            table = new DataTable();
+            adapter.Fill(table);
+
+            dataGridProfiles.DataSource = table;
+            dataGridProfiles.Columns[0].Visible = false;
+            dataGridProfiles.Width = dataGridProfiles.Columns.GetColumnsWidth(DataGridViewElementStates.None) - dataGridProfiles.Columns[0].Width + 18;
+            //dataGridProfiles.AutoSize = true;
+        }
+
+        public void ClearSelectedData()
+        {
+            FirstNameBox.Text = "";
+            LastNameBox.Text = "";
+            UserIDBox.Text = "";
+            HometownBox.Text = "";
+            GenderBox.Text = "";
+            CurrentLocationBox.Text = "";
+            dataGridProfiles.ClearSelection();
+        }
+
+        public Form1()
+        {          
+            InitializeComponent();
+            CreateDataGrid();
+        }
+
+        private void dataGridProfiles_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridProfiles.SelectedRows.Count == 0)
+            {
+                return;
+            }
+
+            //grab userid, first name, last name
+            userID = Convert.ToInt32(dataGridProfiles.SelectedRows[0].Cells[0].Value);            
+            firstName = Convert.ToString(dataGridProfiles.SelectedRows[0].Cells[1].Value);
+            lastName = Convert.ToString(dataGridProfiles.SelectedRows[0].Cells[2].Value);
+
+            //grab hometown, gender, currentlocation
+            command = new MySqlCommand("SELECT Hometown, Gender, CurrentLocation FROM user WHERE UserID = @UserID;", connection);
+            command.Parameters.AddWithValue("@UserID",userID);
+            adapter = new MySqlDataAdapter(command);
+            table = new DataTable();
+            adapter.Fill(table);
+
+            //write into text boxes
+            UserIDBox.Text = Convert.ToString(userID);
+            FirstNameBox.Text = firstName;
+            LastNameBox.Text = lastName;
+
+            HometownBox.Text = table.Rows[0].Field<string>("Hometown");
+            GenderBox.Text = table.Rows[0].Field<string>("Gender");
+            CurrentLocationBox.Text = table.Rows[0].Field<string>("CurrentLocation");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //grab userid
+            userID = Convert.ToInt32(dataGridProfiles.SelectedRows[0].Cells[0].Value);
+
+            //save changes of user info to database
+            command = new MySqlCommand("UPDATE user SET FirstName = @FirstName, LastName = @LastName, Hometown = @Hometown, Gender = @Gender, CurrentLocation = @CurrentLocation WHERE UserID = @UserID", connection);
+            command.Parameters.AddWithValue("@FirstName", FirstNameBox.Text);
+            command.Parameters.AddWithValue("@LastName", LastNameBox.Text);
+            command.Parameters.AddWithValue("@UserID", userID);
+            command.Parameters.AddWithValue("@Hometown", HometownBox.Text);
+            command.Parameters.AddWithValue("@Gender", GenderBox.Text);
+            command.Parameters.AddWithValue("@CurrentLocation", CurrentLocationBox.Text);
+            command.ExecuteNonQuery();
+
+            //refresh table
+            CreateDataGrid();
+
+            //Clear Selection
+            ClearSelectedData();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Clear Selection
+            ClearSelectedData();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //grab last id 
+            command = new MySqlCommand("SELECT UserID FROM user ORDER BY UserID DESC", connection);
+            adapter = new MySqlDataAdapter(command);
+            table = new DataTable();
+            adapter.Fill(table);
+            nextID = Convert.ToInt32(table.Rows[0].Field<int>("UserID")) + 1;
+
+            //create new record
+            command = new MySqlCommand("INSERT INTO user (UserID, FirstName, LastName, Hometown, Gender, CurrentLocation) VALUES (@UserID, @FirstName, @LastName, @Hometown, @Gender, @CurrentLocation)", connection);
+            command.Parameters.AddWithValue("@FirstName", FirstNameBox.Text);
+            command.Parameters.AddWithValue("@LastName", LastNameBox.Text);
+            command.Parameters.AddWithValue("@UserID", nextID);
+            command.Parameters.AddWithValue("@Hometown", HometownBox.Text);
+            command.Parameters.AddWithValue("@Gender", GenderBox.Text);
+            command.Parameters.AddWithValue("@CurrentLocation", CurrentLocationBox.Text);
+            command.ExecuteNonQuery();
+
+            //refresh table
+            CreateDataGrid();
+
+            //Clear Selection
+            ClearSelectedData();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            connection.Close();
+        }
+    }
+}
